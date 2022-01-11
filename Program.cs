24b5b0102c11a002d721dd58d05291ae2e8a6193
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2022-01-11 12:21:26 +0600

    Creating Your Own Namespace

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Test
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            Console.WriteLine("This is a .NET test code.");
+
+            MyInfo.Info data = new MyInfo.Info();
+
+            data.INFO();
+
+        }
+    }
+}
+namespace MyInfo
+{
+    class Info
+    {
+       public void INFO()
+        {
+            Console.WriteLine("My name is Anzamul Haque Akash and I'm 24.");
+        }
+    }
+}
